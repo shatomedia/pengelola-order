81edb2388590f199696c65c5772e4441c3252b24
faik-irkham <faikirkham@gmail.com>
2023-11-13 20:18:20 +0700

    create permission

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,22 +3,91 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('permission:permission', ['only' => ['index','store']]);
-        $this->middleware('permission:permission-edit', ['only' => ['edit','update']]);
+        $this->middleware('permission:permission', ['only' => ['index', 'store']]);
+        $this->middleware('permission:permission-edit', ['only' => ['edit', 'update']]);
         $this->middleware('permission:permission-delete', ['only' => ['destroy']]);
     }
 
     public function index()
     {
         $title = 'Permission';
-        $permissions = Permission::get();
+        $categories = Permission::select('category')->groupBy('category')->get();
+        $permissions = Permission::orderBy('category', 'asc')
+            ->paginate(10);
 
-        return view('dashboard.permission.index', compact('title','permissions'));
+        return view('permission.index', compact('title', 'permissions', 'categories'));
+    }
+
+    public function store(Request $request)
+    {
+        try {
+
+            $validasi_category = $request->kategori_baru ? 'nullable' : 'required';
+            $validasi_kategori_baru = $request->category ? 'nullable' : 'required';
+
+            $request->validate([
+                'name' => 'required|unique:permissions,name',
+                'category' => $validasi_category,
+                'kategori_baru' => $validasi_kategori_baru,
+            ]);
+
+            Permission::create([
+                'name' => $request->name,
+                'category' => $request->category ?? $request->kategori_baru,
+            ]);
+
+            alert()->success('Success', 'Permission created successfully');
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            alert()->error('Error', 'Permission failed to create');
+        }
+
+        return redirect()->back();
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $permission = Permission::findOrFail($id);
+
+            $request->validate([
+                'name' => 'required|unique:permissions,name,' . $permission->id,
+                'category' => 'required',
+            ]);
+
+            $permission->update([
+                'name' => $request->name,
+                'category' => $request->category,
+            ]);
+
+            alert()->success('Success', 'Permission updated successfully');
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            alert()->error('Error', 'Permission failed to update');
+        }
+
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $permission = Permission::findOrFail($id);
+            $permission->delete();
+
+            alert()->success('Success', 'Permission deleted successfully');
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            alert()->error('Error', 'Permission failed to delete');
+        }
+
+        return redirect()->back();
     }
 }
---
+++ resources/views/permission/index.blade.php
@@ -0,0 +1,197 @@
+@extends('layouts.master')
+
+@section('content')
+    <div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4 mx-4">
+                    <div class="card-header pb-0">
+                        <div class="d-flex flex-row justify-content-between">
+                            <div>
+                                <h5 class="mb-0">Permission</h5>
+                            </div>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn bg-gradient-warning" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                                Tambah Permission
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Tambah Permission</h5>
+                                            <button type="button" class="btn-close text-dark" data-bs-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <form action="{{ route('permission.store') }}" method="post">
+                                            @csrf
+
+                                            <div class="modal-body">
+                                                @if ($errors->any())
+                                                    <div class="alert alert-danger">
+                                                        <ul>
+                                                            @foreach ($errors->all() as $error)
+                                                                <li>{{ $error }}</li>
+                                                            @endforeach
+                                                        </ul>
+                                                    </div>
+                                                @endif
+                                                <div class="mb-3">
+                                                    <label for="name" class="form-label">Nama</label>
+                                                    <input type="text" class="form-control" id="name"
+                                                        name="name">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="kategori" class="form-label">Kategori</label>
+                                                    <select name="category" id="kategori" class="form-control">
+                                                        <option value="">Pilih</option>
+                                                        @foreach ($categories as $category)
+                                                            <option value="{{ $category->category }}"
+                                                                {{ old('category') == $category->category ? 'selected' : '' }}>
+                                                                {{ $category->category }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="kategori_baru" class="form-label">Kategori Baru</label>
+                                                    <input type="text" class="form-control" id="kategori_baru"
+                                                        name="kategori_baru">
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn bg-gradient-secondary"
+                                                    data-bs-dismiss="modal">Tutup</button>
+                                                <button type="submit" class="btn bg-gradient-primary">Simpan</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center mb-0">
+                                <thead>
+                                    <tr>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            No
+                                        </th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Nama
+                                        </th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Kateogri
+                                        </th>
+
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Aksi
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($permissions as $no => $permission)
+                                        <tr>
+                                            <td>
+                                                <p class="text-sm font-weight-bold mb-0 px-3">{{ $no + 1 }}</p>
+                                            </td>
+                                            <td>
+                                                <p class="text-center text-sm font-weight-bold mb-0">{{ $permission->name }}
+                                                </p>
+                                            </td>
+                                            <td>
+                                                <p class="text-center text-sm font-weight-bold mb-0">
+                                                    {{ $permission->category }}</p>
+                                            </td>
+                                            <td>
+                                                <div class="d-flex justify-content-center">
+                                                    <button type="button" class="btn bg-gradient-warning btn-sm mx-1"
+                                                        data-bs-toggle="modal"
+                                                        data-bs-target="#modalEdit{{ $permission->id }}">
+                                                        Edit
+                                                    </button>
+                                                    <!-- Modal -->
+                                                    <div class="modal fade" id="modalEdit{{ $permission->id }}"
+                                                        tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                                                        aria-hidden="true">
+                                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">Edit
+                                                                        Permission</h5>
+                                                                    <button type="button" class="btn-close text-dark"
+                                                                        data-bs-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <form
+                                                                    action="{{ route('permission.update', $permission->id) }}"
+                                                                    method="POST">
+                                                                    @csrf
+                                                                    @method('PUT')
+
+                                                                    <div class="modal-body">
+                                                                        <div class="mb-3">
+                                                                            <label for="name"
+                                                                                class="form-label">Nama</label>
+                                                                            <input type="text" class="form-control"
+                                                                                id="name" name="name"
+                                                                                value="{{ $permission->name }}">
+                                                                        </div>
+                                                                        <div class="mb-3">
+                                                                            <label for="kategori"
+                                                                                class="form-label">Kategori</label>
+                                                                            <select name="category" id="kategori"
+                                                                                class="form-control">
+                                                                                <option value="">Pilih</option>
+                                                                                @foreach ($categories as $category)
+                                                                                    <option
+                                                                                        value="{{ $category->category }}"
+                                                                                        {{ $permission->category == $category->category ? 'selected' : '' }}>
+                                                                                        {{ $category->category }}</option>
+                                                                                @endforeach
+                                                                            </select>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="button"
+                                                                            class="btn bg-gradient-secondary"
+                                                                            data-bs-dismiss="modal">Tutup</button>
+                                                                        <button type="submit"
+                                                                            class="btn bg-gradient-primary">Simpan</button>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <form action="{{ route('permission.destroy', $permission->id) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit"
+                                                            class="btn bg-gradient-danger btn-sm mx-1">Hapus</button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="justify-content-center pagination">
+                    {{ $permissions->withQueryString()->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
